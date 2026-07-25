ed36a3df96365fa79e8c71b45b5fabb34ea985bf
04abhisek2006 <abhisekpanda366@gmail.com>
2026-07-26 01:28:49 +0530

    Fix Express 5 routing and server.listen

--- backend/index.js
+++ backend/index.js
@@ -18,14 +18,15 @@ app.use('/api/auth',authRouter)
 app.use('/api/message',messageRouter)
 app.use('/api/user',userRouter)
 app.use(express.static(path.join(__dirname,"/frontend/dist")))
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
-})
 app.get('/',(req,res)=>{
     res.send("server is working")
 })
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 const PORT=process.env.PORT||3000
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     dbConnect();
-    console.log(`Working at ${PORT}`)
+    console.log(`Working at ${PORT}`);
 })
