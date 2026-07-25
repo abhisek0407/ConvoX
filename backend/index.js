7b6b608d92087f2623787252ad5b13223b1a99b7
04abhisek2006 <abhisekpanda366@gmail.com>
2026-07-26 01:16:28 +0530

    added socket

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,21 @@ import authRouter from './route/authUser.js'
 import messageRouter from './route/messageRout.js'
 import cookieParser from "cookie-parser";
 import userRouter from './route/userRout.js'
-const app=express();
+import path from "path";
+import {app , server} from './Socket/socket.js'
+
+const __dirname = path.resolve();
+// const app=express();
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRouter)
 app.use('/api/message',messageRouter)
 app.use('/api/user',userRouter)
-
-
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 app.get('/',(req,res)=>{
     res.send("server is working")
 })
